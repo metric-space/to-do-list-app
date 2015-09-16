bcc743943f07ba4892f59b37f2ec2614ce65568b
luke <nerorevenge@gmail.com>
2015-09-15 21:44:11 -0400

    Out with oop
    Kicked out needless objects, shortened everything.
    Testing is pending.

--- orm.py
+++ orm.py
@@ -1,48 +1,45 @@
-import sqlite3
+import sqlite3 as db 
+import operator
 
-// did you really expect a ORM? haha jokes on you motherfucker
+# What does this do?
+# 1. thin abstraction over commonly used commands to be used by py-sqlite
+# 2. this small library encompasses 'the DB primitives'
+ 
+tableLevelCommands = {
+        'createTable'    :  { 'command' : "create table {table}(id integer primary key,note text)",'argsLen':1 } , 
+        'deleteTable'    :  { 'command' : "drop table if exists {table}"                          ,'argsLen':1},
+        'renameTable'    :  { 'command' : "alter table {old_name} rename to {new_name}"           ,'argsLen':2},
+  }
+
+
+commandDictionary = {
+        'listTables'       : { 'command' : "SELECT name FROM sqlite_master where type='table'"   , 'argsLen':0 } ,
+        'getTableContents' : { 'command' : "select * from {table}"                               , 'argsLen':1 } ,
+        'deleteFromTable'  : { 'command' : "delete from {table} where id={id_number}"            , 'argsLen':2 } ,
+        'insertIntoTable'  : { 'command' : "insert into {table} (note) values ({insertStuff})"   , 'argsLen':2 } ,
+        'updateTableEntry' : { 'command' : "update {table} set note={text} where id={id_number}" , 'argsLen':3 },
+}
+
+listOfCommandDicts      = [commandDictionary, tableLevelCommands]
+allowedCommands         = reduce(operator.concat,[x.keys() for x in listOfCommandDicts],[])
+entireCommandDictionary = reduce(lambda x,y:x.update(y) or x,[listOfCommandDicts],{})
+
 
 def getCursor(path):
     #check to see if path exists
-    connection = connect(path)
-    return connection.cursor
+    connection = db.connect(path)
+    return connection.cursor()
 
-def makeTable(cursor,schema):
+def execute(cursor,command,**kwargs):
+    # limits what one can do with sqlite
+    if command in allowedCommands:
+        if len(kwargs.keys()) == entireCommandDictionary[command]['argsLen']:
+            cursor.execute(entireCommandDictionary[command]['command'].format(**kwargs))
+            return cursor.fetchall()
+    return [] # should replace this with something more meaningful
 
-class databaseObject(object):
 
-    schema = "create table  %s (id integer primary key ,note text) "
 
-	def __init__(self,path):
-                connection  = connect(path) 
-                self.cursor =  
-		self.select_table(temp[0][0])
 
-	def list_tables(self):
-		self.cursor.execute(" select name from sqlite_master where type=\'table\' ")
-		temp = self.cursor.fetchall()
-		return temp	
 
-	def select_table(self,name):
-		''' assuming the table exists '''
-		self.table = name
-		
-	def create_new_table(self,name):
-		self.cursor.execute(self.schema %name)
-
-	def list_contents(self):
-		self.cursor.execute("select * from %s " %self.table)
-		return self.cursor.fetchall()
-	
-	def insert(self,id,what):
-		self.cursor.execute("insert into %s (id,note) values (%d,\'%s\')" %(self.table,id,what))
-
-	def update(self,id,what):
-		try:
-			self.cursor.execute("update %s set note=\'%s\' where id=%d" %(self.table,what,id))
-		except:
-			print "something went wrong with the update button"
-
-	def delete(self,id):
-		self.cursor.execute("delete from %s where id = %d" %(self.table,id))
 
